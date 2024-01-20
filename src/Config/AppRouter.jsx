1678c4dcf2e954251d5c831bba975d12b2e6122d
Mr-Haji <abraheemofficial@gmail.com>
2024-01-21 01:45:58 +0500

    Authantication Done

--- src/Config/AppRouter.jsx
+++ src/Config/AppRouter.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RouteList from "./RouteList";
-import PublicRoutes from "./PublicRoutes";
-import ProtectedRoutes from "./ProtectedRoutes";
-import LogIn from "../Screens/LogIn/LogIn";
-import SignUp from "../Screens/SingUp/SignUp";
-import { Home } from "@mui/icons-material";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route element={<PublicRoutes />}>
-          <Route element={<LogIn />} path="/" />
-          <Route element={<SignUp />} path="/singIn" />
-        </Route>
-        <Route element={<ProtectedRoutes />}>
-          <Route element={<Home />} path="/todo" />
-        </Route>
+        {RouteList.map((e, i) => {
+          return <Route key={i} path={e.path} element={e.element} />;
+        })}
       </Routes>
     </Router>
   );
